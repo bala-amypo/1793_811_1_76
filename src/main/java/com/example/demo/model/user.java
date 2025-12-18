028a2eb107266eb3f581161797583aba0c234077
bala-amypo <bala@amypo.in>
2025-12-18 09:05:22 +0000

    User commit

--- src/main/java/com/example/demo/model/user.java
+++ src/main/java/com/example/demo/model/user.java
@@ -1,5 +1,13 @@
 package com.example.demo.model;
 
 import jakarta.persistence.*;
-import java.util.set;
-import 
+import java.util.Set;
+import java.util.HashSet;
+
+@Entity
+@Table(
+    neme = "users",
+    uniqueConstraints = {
+        @uniqueConstraints(columnName = "username")
+    }
+)
