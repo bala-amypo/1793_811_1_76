2d793d38ec5ebaef56fd2ffb86d99bb5b99caf73
bala-amypo <bala@amypo.in>
2025-12-18 09:30:22 +0000

    User commit

--- src/main/java/com/example/demo/model/user.java
+++ src/main/java/com/example/demo/model/user.java
@@ -27,5 +27,10 @@ public class User {
     private String password;
 
     @ManyToMany(fetch = FetchType.EAGER)
-    @J
+    @JoinTable(
+        name = "user_role",
+        joinColumns = @JoinColumn (name = "user_id"),
+        inverseJoincolumns = @JoinColumn(name = "role_id") 
+    )
+    public set<role
 }
