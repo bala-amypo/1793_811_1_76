31918035fe707c539fa3426da8a5eb88217454e6
bala-amypo <bala@amypo.in>
2025-12-27 08:21:38 +0000

    User commit

--- src/main/java/com/example/demo/security/JwtTokenProvider.java
+++ src/main/java/com/example/demo/security/JwtTokenProvider.java
@@ -1,4 +1,12 @@
 package com.example.demo.security;
 
 public class JwtTokenProvider {
+
+    private final String secret;
+    private final long expiry;
+
+    public JwtTokenProvider(String secret, long expiry) {
+        this.secret = secret;
+        this.expiry = expiry;
+    }
 }
