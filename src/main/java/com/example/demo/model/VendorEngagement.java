9f9bdbdf660c0bc5fcf4a342d17d7d4505b6e630
bala-amypo <bala@amypo.in>
2025-12-27 06:32:54 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/VendorEngagement.java
@@ -0,0 +1,44 @@
+package com.example.demo.model;
+
+public class VendorEngagement {
+
+    private Long id;
+    private Long vendorId;
+    private Long employeeId;
+    private String engagementType;
+
+    public VendorEngagement() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getVendorId() {
+        return vendorId;
+    }
+
+    public void setVendorId(Long vendorId) {
+        this.vendorId = vendorId;
+    }
+
+    public Long getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(Long employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getEngagementType() {
+        return engagementType;
+    }
+
+    public void setEngagementType(String engagementType) {
+        this.engagementType = engagementType;
+    }
+}
